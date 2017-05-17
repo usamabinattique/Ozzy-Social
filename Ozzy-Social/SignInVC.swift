28490f5dbd6785da206b28960fbc3d2ff8b188b4
Usama Attique <usama.attique@therightsw.com>
2017-05-17 17:57:38 +0500

    first commit for Ozzy Social

---
+++ Ozzy-Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Ozzy-Social
+//
+//  Created by M.Fasi on 17/5/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
